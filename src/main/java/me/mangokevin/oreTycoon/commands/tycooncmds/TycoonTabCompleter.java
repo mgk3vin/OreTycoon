41d4f9e6aed8df63e506837d715548cb61c7248a
mgk3vin <NicoM.Horn@web.de>
2025-12-22 17:19:29 +0100

    #fixed TabCompletion for /tyoon toggle_all <on/off>

--- src/main/java/me/mangokevin/oreTycoon/commands/tycooncmds/TycoonTabCompleter.java
+++ src/main/java/me/mangokevin/oreTycoon/commands/tycooncmds/TycoonTabCompleter.java
@@ -1,13 +1,13 @@
 package me.mangokevin.oreTycoon.commands.tycooncmds;
 
-import me.mangokevin.oreTycoon.tycoonManagment.TycoonBlockManager;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,10 +25,10 @@ public class TycoonTabCompleter implements TabCompleter {
                     .collect(Collectors.toList());
         }
 
-        if (strings.length == 2 || strings[1].equalsIgnoreCase("toggle_all")) {
+        if (strings.length == 2 && strings[0].equalsIgnoreCase("toggle_all")) {
             List<String> subcommands = Arrays.asList("on", "off");
 
-            return subcommands.stream().filter(string -> string.toLowerCase().startsWith(strings[0].toLowerCase()))
+            return subcommands.stream().filter(string -> string.toLowerCase().startsWith(strings[1].toLowerCase()))
                     .collect(Collectors.toList());
         }
 
